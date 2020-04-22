dd97924103fbd2cc5217629e3f29063b67581b1f
AVA <ava@posovetuy.com>
2020-04-23 00:12:29 +0300

    Initial commit.

--- telebotpogoda.py
+++ telebotpogoda.py
@@ -2,14 +2,10 @@
 
 import telebot
 
-from telebot import apihelper
-
-apihelper.proxy = {'http':'http://10.10.1.10:3128'}
-
 bot = telebot.TeleBot('1292667823:AAHQ9osRe8tl5afJIz0N_YjvkOYcJlo5aWI')
 
 @bot.message_handler(content_types=['text'])
-def echo_message(message):
+def send_echo(message):
     bot.send_message(message.chat.id, message.text)
  
 bot.polling(none_stop = True)
